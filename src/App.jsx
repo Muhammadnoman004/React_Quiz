64a72b73a9cbb538d6de4be3540718a0add0f0e3
Noman Shamim <nomanshamim2004@gmail.com>
2024-01-11 19:22:54 +0500

    Option render done

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
 
   let [questions, setquestion] = useState([]);
-  let [Options , setOptions] = useState([]);
-  let [CurrentQues , setCurrentQues] = useState(0);
+  let [CurrentQues, setCurrentQues] = useState(0);
+  let Options = [];
 
   useEffect(function () {
     getdataApi()
@@ -14,46 +14,36 @@ function App() {
   function getdataApi() {
     fetch('https://the-trivia-api.com/v2/questions')
       .then(res => res.json())
-      // .then(res => setquestion(res))
-      .then(res => {
-        console.log(res)
-        setquestion(res)
-        let incorrectAnswers = res[CurrentQues].incorrectAnswers;
-        let correctAnswer = res[CurrentQues].correctAnswer;
-        incorrectAnswers.push(correctAnswer);
-        Options.push(incorrectAnswers);
-        console.log(Options);
-      })
-
-    
+      .then(res => setquestion(res))
   }
-  if(!questions.length){
+  if (!questions.length) {
     return <h1>loading...</h1>
   }
 
-  function nextQuestion(){
+  function nextQuestion() {
     setCurrentQues(CurrentQues + 1)
-    setOptions(Options + 1)
   }
 
-  // Options.map((OptionsRender , index) =>{
-  //   return <h4 key={index}>{OptionsRender}</h4>
-  // })
+  let incorrectAnswers = questions[CurrentQues].incorrectAnswers;
+  Options.push(...incorrectAnswers);
+  let correctAnswer = questions[CurrentQues].correctAnswer;
+  Options.push(correctAnswer);
 
   return (
     <div className="App">
 
       <h1>Quiz App</h1>
-      
-      <h4>{questions[CurrentQues].question.text}</h4>
-      <button onClick={nextQuestion}>Next</button>
-      <button style={{display: "none"}}>Restart</button>
-    <ul>
 
-    {
-      <li>{Options}</li>
-    }
-    </ul>
+      <h4>{questions[CurrentQues].question.text}</h4>
+
+      {
+        Options.map((OptionsRender, index) => {
+          return <button className='options' key={index}>{OptionsRender}</button>
+        })
+      }
+      <br />
+      <button onClick={nextQuestion}>Next</button>
+      <button style={{ display: "none" }}>Restart</button>
 
     </div>
   );
